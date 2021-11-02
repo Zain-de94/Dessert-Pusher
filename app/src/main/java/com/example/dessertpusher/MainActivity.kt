89b5cb06c8f82f8302f9842de71e150b7f79c24c
Zain-de94 <71317396+Zain-de94@users.noreply.github.com>
2021-11-02 11:55:54 +0530

    Dessert-Pusher-Step-1-Solution-Logging-The-Callbacks

--- app/src/main/java/com/example/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/dessertpusher/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dessertpusher
 import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -11,7 +12,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleObserver
 import com.example.dessertpusher.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() , LifecycleObserver{
+class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
@@ -46,9 +47,11 @@ class MainActivity : AppCompatActivity() , LifecycleObserver{
     )
     private var currentDessert = allDesserts[0]
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("MainActivity", "onCreate called")
+
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
@@ -63,6 +66,7 @@ class MainActivity : AppCompatActivity() , LifecycleObserver{
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
     }
+
     /**
      * Updates the score when the dessert is clicked. Possibly shows a new dessert.
      */
@@ -113,8 +117,10 @@ class MainActivity : AppCompatActivity() , LifecycleObserver{
         try {
             startActivity(shareIntent)
         } catch (ex: ActivityNotFoundException) {
-            Toast.makeText(this, getString(R.string.sharing_not_available),
-                Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this, getString(R.string.sharing_not_available),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
@@ -128,5 +134,10 @@ class MainActivity : AppCompatActivity() , LifecycleObserver{
             R.id.shareMenuButton -> onShare()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity","onStart called")
     }
 }
